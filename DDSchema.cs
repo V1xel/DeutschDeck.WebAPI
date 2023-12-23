9e6ee15decc39c546d1abb926a48745affba0a97
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-23 15:22:40 +0100

    Feature: Add graphql support
    commit 99f8fc992477b2f6642bec621ed77feebdea38fe
    Author: Vitalii Koliesnik <v1xel@outlook.de>
    Date:   Sat Dec 23 15:21:45 2023 +0100
    
        Added graphql package, default route, playground, empty schema

---
+++ DDSchema.cs
@@ -0,0 +1,11 @@
+﻿using GraphQL.Types;
+
+namespace DeutschDeck.WebAPI
+{
+    public class DDSchema : Schema
+    {
+        public DDSchema(IServiceProvider provider) : base(provider)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,38 @@
+using DeutschDeck.WebAPI;
+using dotenv.net;
+using GraphQL;
+
+var envVars = DotEnv.Read();
+var WEB_APP_ENDPOINT = envVars["WEB_APP_ENDPOINT"];
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddGraphQL((builder) =>
+{
+    builder.AddSystemTextJson();
+    builder.AddErrorInfoProvider((opts, serviceProvider) => opts.ExposeExceptionDetails = true);
+    builder.AddSchema<DDSchema>();
+});
+
+builder.Services.AddSingleton<DDSchema>();
+
+var OriginsPolicy = "_AllowLocalOrigin";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(OriginsPolicy,
+                      builder =>
+                      {
+                          builder.WithOrigins(WEB_APP_ENDPOINT)
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod();
+                      });
+});
+
 var app = builder.Build();
+
+app.UseCors(OriginsPolicy);
 app.UseHttpsRedirection();
+
+app.UseGraphQL("/graphql");
+app.UseGraphQLAltair();
 app.Run();
