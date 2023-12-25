666e336bba9cc97ffd58ca462204d3c8d0111176
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-25 10:21:08 +0100

    Feature: Add email delivery
    commit 57b764753d5f592c448e2e64ccabb66aa7621982
    Author: Vitalii Koliesnik <v1xel@outlook.de>
    Date:   Sun Dec 24 19:35:32 2023 +0100
    
        Added template, template provider, adapter for MailerSend

---
+++ Emails/MailerSendAdapter.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp.Authenticators;
+using RestSharp;
+using GraphQLParser;
+using Microsoft.Extensions.Configuration;
+
+namespace DeutschDeck.WebAPI.Emails
+{
+    public class MailerSendAdapter(EmailDeliveryAdapterConfiguration configuration, SignupTemplateProvider provider) : IEmailDeliveryAdapter
+    {
+        record MailerSendRerson(string email);
+        record MailerSendRequestBody(MailerSendRerson from, List<MailerSendRerson> to, string subject, string html);
+
+        private RestClient _client = new RestClient(new RestClientOptions(configuration.endpoint)
+        {
+            Authenticator = new JwtAuthenticator(configuration.token)
+        });
+
+        public async Task SendSignupResponse(string recipient, string password)
+        {
+            var request = new RestRequest();
+            request.AddJsonBody(new MailerSendRequestBody(
+                new MailerSendRerson(configuration.sender), 
+                [new MailerSendRerson(recipient)],
+                provider.Subject, 
+                provider.GetFilledTemplate(password)
+                ));
+            await _client.PostAsync(request);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using DeutschDeck.WebAPI;
 using DeutschDeck.WebAPI.Database;
+using DeutschDeck.WebAPI.Emails;
 using dotenv.net;
 using GraphQL;
 using Microsoft.EntityFrameworkCore;
@@ -13,6 +14,12 @@ var POSTGRES_USER = envVars["POSTGRES_USER"];
 var POSTGRES_PASSWORD = envVars["POSTGRES_PASSWORD"];
 var POSTGRES_CONNECTION = string.Format("Host={0};Database={1};Username={2};Password={3}", POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD);
 
+var EMAIL_DELIVERY_TOKEN = envVars["EMAIL_DELIVERY_TOKEN"];
+var EMAIL_DELIVERY_ENDPOINT = envVars["EMAIL_DELIVERY_ENDPOINT"];
+var EMAIL_DELIVERY_SENDER = envVars["EMAIL_DELIVERY_SENDER"];
+var EMAIL_DELIVERY_SIGNIN_TEMPLATE = envVars["EMAIL_DELIVERY_SIGNIN_TEMPLATE"];
+var EMAIL_DELIVERY_SIGNIN_SUBJECT = envVars["EMAIL_DELIVERY_SIGNIN_SUBJECT"];
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddGraphQL((builder) =>
 {
@@ -20,6 +27,11 @@ builder.Services.AddGraphQL((builder) =>
     builder.AddErrorInfoProvider((opts, serviceProvider) => opts.ExposeExceptionDetails = true);
     builder.AddSchema<DDSchema>();
 });
+
+builder.Services.AddSingleton(new SignupTemplateProviderConfiguration(EMAIL_DELIVERY_SIGNIN_TEMPLATE, EMAIL_DELIVERY_SIGNIN_SUBJECT));
+builder.Services.AddSingleton<SignupTemplateProvider>();
+builder.Services.AddSingleton(new EmailDeliveryAdapterConfiguration(EMAIL_DELIVERY_TOKEN, EMAIL_DELIVERY_ENDPOINT, EMAIL_DELIVERY_SENDER));
+builder.Services.AddSingleton<IEmailDeliveryAdapter, MailerSendAdapter>();
 
 builder.Services.AddSingleton<DDSchema>();
 builder.Services.AddDbContext<DDContext>(options => options.UseNpgsql(POSTGRES_CONNECTION), ServiceLifetime.Singleton);
