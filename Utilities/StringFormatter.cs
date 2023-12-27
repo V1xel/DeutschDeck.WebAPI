2bbb45822a57eb28ddb100e76af29a0ec10ce883
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-27 11:54:33 +0100

    Added string formatter utility

---
+++ Utilities/StringFormatter.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DeutschDeck.WebAPI.Utilities
+{
+    public class StringFormatter
+    {
+        public static string Format(string template, params string[] values)
+        {
+            return Regex.Replace(template, @"{(\d+)}", match => {
+                int index = int.Parse(match.Groups[1].Value);
+                return index < values.Length ? values[index] : match.Value;
+            });
+        }
+    }
+}
