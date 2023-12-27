5da1b3f5a72be7554d4bfcd4ce9ed861ca7c7c97
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-27 11:59:28 +0100

    Added mocked email delivery to save request limits

---
+++ Emails/MockedEmailDelivery.cs
@@ -0,0 +1,12 @@
+﻿
+namespace DeutschDeck.WebAPI.Emails
+{
+    public class MockedEmailDelivery(EmailDeliveryAdapterConfiguration configuration, SignupTemplateProvider provider) : IEmailDeliveryAdapter
+    {
+        public Task SendSignupResponse(string recipient, string password)
+        {
+            Console.WriteLine("IEmailDeliveryAdapter:: recipient:{1} password:{2}", recipient, password);
+            return Task.CompletedTask;
+        }
+    }
+}
