94dd3d60b8d0d7637db0b539714092feea3fa644
Vitalii Koliesnik <v1xel@outlook.de>
2023-12-27 10:13:48 +0100

    Added CreateUser migration

--- Database/DDContext.cs
+++ Database/DDContext.cs
@@ -1,8 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DeutschDeck.WebAPI.Database.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace DeutschDeck.WebAPI.Database
 {
     public class DDContext(DbContextOptions<DDContext> options) : DbContext(options)
     {
+        public DbSet<UserEntity> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<UserEntity>().Property(p => p.Id).ValueGeneratedOnAdd();
+            builder.Entity<UserEntity>().HasIndex(u => u.Email).IsUnique();
+        }
     }
 }
